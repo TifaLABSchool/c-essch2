43ca32ec3be952f72fc696cefaec408b073dc67f
Mostafa El-Tayeb <mostafa87eltayeb@gmail.com>
2017-07-13 14:29:43 +0200

    Add files via upload

---
+++ TreeViewDemo/TreeViewDemo/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeViewDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*Load Location path*/
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+           if(result == DialogResult.OK)
+            {
+           textBox1.Text = fbd.SelectedPath;
+            }
+        }
+        /*Show Files And Folders In treeView */
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ListFoldersInTreeView(textBox1.Text);
+        }
+
+        private void ListFoldersInTreeView(string Path)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(Path);
+            treeView1.Nodes.Add(CreateFoldersNode(directoryInfo));
+        }
+
+        private TreeNode CreateFoldersNode(DirectoryInfo directoryInfo)
+        {
+            TreeNode tn = new TreeNode(directoryInfo.Name);
+            foreach(var subfolder in directoryInfo.GetDirectories())
+            {
+                tn.Nodes.Add(CreateFoldersNode(subfolder));
+
+            }
+            foreach (var subfolderfiles in directoryInfo.GetFiles())
+            {
+                tn.Nodes.Add(new TreeNode(subfolderfiles.Name));
+
+            }
+            return tn;
+        }
+    }
+}
---
+++ TreeViewDemo/TreeViewDemo/TreeViewDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeViewDemo
+{
+    public partial class TreeViewDemo : Form
+    {
+        public TreeViewDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void TreeViewDemo_Load(object sender, EventArgs e)
+        {
+            TreeNode DepTreenode = new TreeNode("Department");
+            DepTreenode.Nodes.Add("HR");
+            DepTreenode.Nodes.Add("IT");
+            DepTreenode.Nodes.Add("SERVICES");
+            TreeNode empTreenode = new TreeNode("Employee");
+            empTreenode.Nodes.Add("Mostafa");
+            empTreenode.Nodes.Add("Ahmed");
+            empTreenode.Nodes.Add("mohammed");
+            treeView1.Nodes.Add(DepTreenode);
+            treeView1.Nodes.Add(empTreenode);
+        }
+        /*Remove node From Tree View */
+        private void button2_Click(object sender, EventArgs e)
+        {
+            treeView1.SelectedNode.Remove();
+        }
+        /**Add New Node To TreeView */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes[comboBox1.SelectedIndex].Nodes.Add(textBox1.Text);
+        }
+    }
+}
